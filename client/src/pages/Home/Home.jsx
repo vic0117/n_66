42ba28d2d8d94bca8ed73189e4e2fc24f1e32776
T-Bone1902 <shoruken1902@gmail.com>
2019-11-11 20:03:48 +0800

    修改login頁面

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 // Components
 
-import HomeNavBar from "../../components/HomeNavBar/HomeNavBar";
-import HomeMainCarousel from "../../components/HomeMainCarousel/HomeMainCarousel";
-import FilterHome from '../../components/FilterHome/FilterHome';
-import ReviewCarousel from '../../components/ReviewCarousel/ReviewCarousel'
-
+// import HomeNavBar from "../../components/HomeNavBar/HomeNavBar";
+// import HomeMainCarousel from "../../components/HomeMainCarousel/HomeMainCarousel";
+// import FilterHome from '../../components/FilterHome/FilterHome';
+// import ReviewCarousel from '../../components/ReviewCarousel/ReviewCarousel'
+import Login from '../Login/Login';
 
 class Home extends Component {
   state = {};
   render() {
     return (
       <>
-        
-        <HomeNavBar/>
-        <HomeMainCarousel />
-        <FilterHome/>
-        <ReviewCarousel/>
+        <Login/>
+        {/* <HomeNavBar/> */}
+        {/* <HomeMainCarousel /> */}
+        {/* <FilterHome/> */}
+        {/* <ReviewCarousel/> */}
       </>
     );
   }
