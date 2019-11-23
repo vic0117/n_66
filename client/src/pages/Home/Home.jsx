245001855c9c6f7e720511523c1de38a71535fa0
Yi-Cheng Tseng <jordantseng1024@gmail.com>
2019-11-23 22:03:51 +0800

    1122

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,37 +8,43 @@ import HomeTravelCarousel from "../../components/HomeTravelCarousel/HomeTravelCa
 import HomeAdviser from "../../components/HomeAdviser/HomeAdviser";
 import HomeEarth from "../../components/HomeEarth/HomeEarth";
 import Footer from "../../components/Footer/Footer";
-import message from './images/comment-dots-solid.svg'
-import './Home.scss'
+import message from "./images/comment-dots-solid.svg";
+import "./Home.scss";
 // import Join from "../socketClient/Join";
 import Chat from "../socketClient/Chat";
 class Home extends Component {
   state = {
-	  system:false,
-	  name:'',
-	  room:''
+    system: false,
+    name: "",
+    room: ""
   };
 
-  
   render() {
-	  const {currentUser} = this.props;
-	  console.log(currentUser)
+    const { currentUser } = this.props;
+    console.log(currentUser);
     return (
       <>
-			<div className="MessageButton" onClick={()=>this.setState({system:!this.state.system})}>
-				<img src={message} alt='message'/>	
-			</div>
-			<div className={this.state.system ? ' messageIn' : 'messageInOpen messageIn'}>
-				<Chat/>
-			</div>
-        <HomeNavBar currentUser={this.props.currentUser}/>
+        <div
+          className="MessageButton"
+          onClick={() => this.setState({ system: !this.state.system })}
+        >
+          <img src={message} alt="message" />
+        </div>
+        <div
+          className={
+            this.state.system ? " messageIn" : "messageInOpen messageIn"
+          }
+        >
+          <Chat />
+        </div>
+        <HomeNavBar currentUser={this.props.currentUser} />
         <HomeMainCarousel />
-        <HomeFilter 
-			  HomeSelect1={this.props.HomeSelect1}
-			  HomeSelect2={this.props.HomeSelect2}
-			  HomeSelect3={this.props.HomeSelect3}
-			  HomeSearch = {this.props.HomeSearch}
-		  />
+        <HomeFilter
+          HomeSelect1={this.props.HomeSelect1}
+          HomeSelect2={this.props.HomeSelect2}
+          HomeSelect3={this.props.HomeSelect3}
+          HomeSearch={this.props.HomeSearch}
+        />
         <HomeTravelCarousel />
         <HomeAdviser />
         <HomeEarth />
