f1030b675c3e8602b73d726c85d63227938a8f1e
Yi-Cheng Tseng <jordantseng1024@gmail.com>
2019-11-29 21:13:19 +0800

    1128

--- server/src/products/products.js
+++ server/src/products/products.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 const bluebird = require("bluebird");
 
 const db = mysql.createConnection({
-  // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
   host: "localhost",
   user: "root",
   password: "root",
